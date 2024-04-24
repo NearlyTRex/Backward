5a1b1c90da9dd18008e7e6d31c2345ad4646569e
Luna White <NearlyTRex@users.noreply.github.com>
2024-04-24 12:36:18 -0400

    Update backward.hpp
    Temp commenting out code that breaks windows build until an official fix comes from upstream

--- backward.hpp
+++ backward.hpp
@@ -4339,9 +4339,9 @@ public:
     _set_abort_behavior(0, _WRITE_ABORT_MSG | _CALL_REPORTFAULT);
 
     std::set_terminate(&terminator);
-#ifndef BACKWARD_ATLEAST_CXX17
-    std::set_unexpected(&terminator);
-#endif
+//#ifndef BACKWARD_ATLEAST_CXX17
+//    std::set_unexpected(&terminator);
+//#endif
     _set_purecall_handler(&terminator);
     _set_invalid_parameter_handler(&invalid_parameter_handler);
   }
